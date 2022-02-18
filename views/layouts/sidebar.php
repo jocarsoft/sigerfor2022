8e67b93a6dd44fcd4960b5ce59c464d934dc6eab
jocarsoft <jorge.sangama.flores@gmail.com>
2022-02-18 12:21:28 -0500

    link dashboard en el sidebar

--- views/layouts/sidebar.php
+++ views/layouts/sidebar.php
@@ -47,7 +47,8 @@
                 //    ['label' => 'Simple Link', 'icon' => 'th', 'badge' => '<span class="right badge badge-danger">New</span>'],
                 //    ['label' => 'Yii2 PROVIDED', 'header' => true],
                 //    ['label' => 'Login', 'url' => ['site/login'], 'icon' => 'sign-in-alt', 'visible' => Yii::$app->user->isGuest],
-                    ['label' => 'Gii',  'icon' => 'file-code', 'url' => ['/gii'], 'target' => '_blank'],
+                ['label' => 'Dashboard',  'icon' => 'file-code', 'url' => ['site/index']],
+                ['label' => 'Gii',  'icon' => 'file-code', 'url' => ['/gii'], 'target' => '_blank'],
                 //    ['label' => 'Debug', 'icon' => 'bug', 'url' => ['/debug'], 'target' => '_blank'],
                //     ['label' => 'MULTI LEVEL EXAMPLE', 'header' => true],
                    
