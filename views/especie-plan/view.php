ea60027dded61f7488d51b7b8d25a703a1fa9e82
josehumberto1995 <lopezolortegui1995@gmail.com>
2022-02-28 11:39:08 -0500

    modificando

--- views/especie-plan/view.php
+++ views/especie-plan/view.php
@@ -7,7 +7,7 @@ use yii\widgets\DetailView;
 /* @var $model app\models\EspeciePlan */
 
 $this->title = $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Especie Plans', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => 'Especie Planes', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
