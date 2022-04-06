03e5a4a81d0f3ec21d97d9d7154b0e0dc75b2d3b
josehumberto1995 <lopezolortegui1995@gmail.com>
2022-04-06 10:55:47 -0500

    Btn sgte - wizard.js

--- backend/views/guia/_form.php
+++ backend/views/guia/_form.php
@@ -302,4 +302,4 @@ use backend\models\Constantesmod;
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
-<script src="js/wizard_js.js"></script>
+<script src="../js/wizard_js.js"></script>
--- backend/views/permisosmod/_form.php
+++ backend/views/permisosmod/_form.php
@@ -349,5 +349,5 @@ use backend\models\Constantesmod;
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
-<script src="js/wizard_js.js"></script>
+<script src="../js/wizard_js.js"></script>
 
