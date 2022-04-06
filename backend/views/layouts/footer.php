6e38f09f26b4f9db45b36b43838c9e7ecc2f7e50
josehumberto1995 <lopezolortegui1995@gmail.com>
2022-04-06 10:20:51 -0500

    Modificacion view

--- backend/views/layouts/footer.php
+++ backend/views/layouts/footer.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
+    <strong>Copyright &copy; GERFOR-LORETO 2022 <a href="#">SIGERFOR</a>.</strong>
+    Todos los derechos reservados.
     <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 3.1.0
+        <b>Version</b> 1.0
     </div>
 </footer>
--- backend/views/layouts/sidebar.php
+++ backend/views/layouts/sidebar.php
@@ -2,7 +2,7 @@
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
         <img src="<?=$assetDir?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <span class="brand-text font-weight-light">SIGERFOR</span>
     </a>
 
     <!-- Sidebar -->
@@ -13,7 +13,7 @@
                 <img src="<?=$assetDir?>/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Alexander Pierce</a>
+                <a href="#" class="d-block">Bienvenido</a>
             </div>
         </div>
 
@@ -36,19 +36,19 @@
             echo \hail812\adminlte\widgets\Menu::widget([
                 'items' => [
                     [
-                        'label' => 'Starter Pages',
+                        'label' => 'Dashboard',
                         'icon' => 'tachometer-alt',
                         'badge' => '<span class="right badge badge-info">2</span>',
                         'items' => [
-                            ['label' => 'Active Page', 'url' => ['site/index'], 'iconStyle' => 'far'],
-                            ['label' => 'Inactive Page', 'iconStyle' => 'far'],
+                            ['label' => 'Inicio', 'url' => ['site/index'], 'iconStyle' => 'far'],
+                           //['label' => 'Inactive Page', 'iconStyle' => 'far'],
                         ]
                     ],
-                    ['label' => 'Simple Link', 'icon' => 'th', 'badge' => '<span class="right badge badge-danger">New</span>'],
+                    //['label' => 'Simple Link', 'icon' => 'th', 'badge' => '<span class="right badge badge-danger">New</span>'],
                     ['label' => 'Yii2 PROVIDED', 'header' => true],
-                    ['label' => 'Login', 'url' => ['site/login'], 'icon' => 'sign-in-alt', 'visible' => Yii::$app->user->isGuest],
-                    ['label' => 'Gii',  'icon' => 'file-code', 'url' => ['/gii'], 'target' => '_blank'],
-                    ['label' => 'Debug', 'icon' => 'bug', 'url' => ['/debug'], 'target' => '_blank'],
+                    //['label' => 'Login', 'url' => ['site/login'], 'icon' => 'sign-in-alt', 'visible' => Yii::$app->user->isGuest],
+                    //['label' => 'Gii',  'icon' => 'file-code', 'url' => ['/gii'], 'target' => '_blank'],
+                    //['label' => 'Debug', 'icon' => 'bug', 'url' => ['/debug'], 'target' => '_blank'],
                     ['label' => 'GESTON DE USUARIOS', 'header' => true],
                     ['label' => 'Usuarios', 'url' => ['/user/user/index'], 'iconStyle' => 'far'],
                     [
@@ -60,7 +60,7 @@
                             ['label' => 'Asignación de roles', 'url' => ['/rbac/assignment'], 'iconStyle' => 'far'],                           
                         ]
                     ],
-                    ['label' => 'Dashboard',  'icon' => 'file-code', 'url' => ['/site/index']],
+                    //['label' => 'Dashboard',  'icon' => 'file-code', 'url' => ['/site/index']],
                     ['label' => 'Gii',  'icon' => 'file-code', 'url' => ['/gii'], 'target' => '_blank'],
                     ['label' => 'TH', 'header' => true],
                     ['label' => 'Permisos', 'url' => ['/permisosmod/index']],
