99961c2bc68f2ce53fad1e9ece642b0abb0c4487
xaralio9 <82296115+xaralio9@users.noreply.github.com>
2022-04-07 13:17:40 -0500

    arreglos

--- backend/views/autorizacionesmod/_form.php
+++ backend/views/autorizacionesmod/_form.php
@@ -42,7 +42,20 @@ use backend\models\Constantesmod;
                                     <div class="row">
 
                                         <div class="col-md-6">
-                                        <?= $form->field($model, 'SEDDSC')->textInput() ?>
+                                        
+                                        <?php 
+                                                //para crear campo combo
+                                                $datos = Constantesmod::find()
+                                                ->where(['campo' => 'SEDDSC','tabla'=>'ddguia'])
+                                                ->all();
+                                                $listData=ArrayHelper::map($datos,'CODE','DESCRIPCION');
+                                                echo $form->field($model, 'SEDDSC')->dropDownList(
+                                                $listData,
+                                                ['prompt'=>'Seleccionar...']
+                                                    );    
+                                            ?>
+
+
                                         </div>
 
                                         <div class="col-md-6">
